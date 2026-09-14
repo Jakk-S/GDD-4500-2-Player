0ff3bac1781fe867b03dad752161df0ef08a087e
Jakk-S <jsteinbr@uccs.edu>
2026-09-13 23:03:16 -0600

    Damage Added
    Weapons do damage
    players take damage and have health
    prefab updated with tags for collision detection

--- Multiplayer - 2 Player/Assets/Scripts/PlayerInfo.cs
+++ Multiplayer - 2 Player/Assets/Scripts/PlayerInfo.cs
@@ -5,4 +5,21 @@ public enum PlayerSlot { Player1, Player2 }
 public class PlayerInfo : MonoBehaviour
 {
     [field:SerializeField] public PlayerSlot PlayerSlot { get; private set; }
+
+    [SerializeField] [Range(0, 100)] private int maxHealth = 100;
+    public int currentHeatlh;
+
+    private void Start()
+    {
+        currentHeatlh = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHeatlh -= damage;
+        if (currentHeatlh <= 0)
+        {
+            Debug.Log("Game Over!!");
+        }
+    }
 }
--- Multiplayer - 2 Player/Assets/Scripts/WeaponStuff.cs
+++ Multiplayer - 2 Player/Assets/Scripts/WeaponStuff.cs
@@ -2,8 +2,14 @@ using UnityEngine;
 
 public class WeaponStuff : MonoBehaviour
 {
+    [SerializeField] [Range(0, 5)] private int damage = 1;
+
     void OnCollisionEnter2D(Collision2D collision)
     {
         transform.parent.parent.GetComponent<PlayerMovement>().ReverseRotate();
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.GetComponent<PlayerInfo>().TakeDamage(damage);
+        }
     }
 }
